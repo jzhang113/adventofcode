e43b163f636895cf807ba384bd8963603173e751
jzhang113 <jiahuaz01@gmail.com>
2022-12-01 22:53:00 -0600

    Add solution template

---
+++ 2022/solutions/template.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'active_support'
+require 'active_support/all'
+require_relative '../input'
+
+class Solution
+  include Input
+
+  def part1
+
+  end
+
+  def part2
+
+  end
+end
+
+soln = Solution.new
+puts soln.part1
+puts soln.part2
