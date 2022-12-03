fc029fe46bba23444db29377e97cb2c9794b15a1
jzhang113 <jiahuaz01@gmail.com>
2022-12-03 01:44:10 -0600

    Automatically display any instance variables defined

--- 2022/solutions/base_solution.rb
+++ 2022/solutions/base_solution.rb
@@ -2,9 +2,12 @@
 
 require 'active_support'
 require 'active_support/all'
+require 'byebug'
 
 class BaseSolution
   # include Input in the subclass to make available an `input` accessor to the data
+
+  EXCLUDED_INSTANCE_VARS = ['input']
 
   def part1
     raise 'Implement your solution to part one of the problem'
@@ -19,5 +22,18 @@ class BaseSolution
     puts "Running solution for day #{day_num}"
     puts "Part 1: #{part1}"
     puts "Part 2: #{part2}"
+
+    dump
+  end
+
+  def dump
+    vars = instance_values.except(*EXCLUDED_INSTANCE_VARS)
+    puts 'Instance variables:' if vars.present?
+
+    vars.each do |key, value|
+      puts "#{key} => #{value.inspect}"
+    end
+
+    nil
   end
 end
