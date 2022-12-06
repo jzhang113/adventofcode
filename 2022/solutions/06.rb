44a9344bac4a9029eac66419db90b4e033919879
jzhang113 <jiahuaz01@gmail.com>
2022-12-05 23:33:02 -0600

    Day 6

---
+++ 2022/solutions/06.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative './base_solution'
+require_relative '../input'
+
+class Solution < BaseSolution
+  include Input
+
+  def part1
+    input[0].length.times do |i|
+      phrase = input[0][i..(i + 3)]
+      letters = phrase.split('')
+      return (i + 4) if letters == letters.uniq
+    end
+  end
+
+  def part2
+    input[0].length.times do |i|
+      phrase = input[0][i..(i + 13)]
+      letters = phrase.split('')
+      return (i + 14) if letters == letters.uniq
+    end
+  end
+end
+
+Solution.new.run
